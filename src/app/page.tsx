02adb0102f08ccf7704938ce8c5af8b9606ffd4d
ColourfulMelon <jordynnitch@gmail.com>
2025-01-20 14:09:55 +0200

    add header and footer and basic components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello world!</p>
-
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center justify-between p-24">
+            <p>hello world!</p>
+        
+        </main>
+    );
 }
