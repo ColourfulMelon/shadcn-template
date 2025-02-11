c41f6c0036c4778a8c71d7f2e841e41f918f9fd3
ColourfulMelon <jordynnitch@gmail.com>
2025-02-11 17:37:20 +0200

    move jsonld in component

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ import { Twitter } from 'next/dist/lib/metadata/types/twitter-types';
 
 const inter = Inter({ subsets: ['latin'] });
 
-
 const openGraph: OpenGraph = {
     title: 'OpenGraphTitle',
     description: 'OpenGraphDescription',
@@ -60,6 +59,7 @@ const openGraph: OpenGraph = {
     
     
 }
+
 // todo: update metadata
 export const metadata: Metadata = {
     title: 'title',
@@ -114,13 +114,20 @@ export const metadata: Metadata = {
     // },
 };
 
-const jsonLD = {"@context":"https://schema.org","@graph":[{"@type":["Person","Organization"],"@id":"https://aventus.io/#person","name":"Aventus","logo":{"@type":"ImageObject","@id":"https://aventus.io/#logo","url":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","contentUrl":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","caption":"Aventus","inLanguage":"en-GB"},"image":{"@type":"ImageObject","@id":"https://aventus.io/#logo","url":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","contentUrl":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","caption":"Aventus","inLanguage":"en-GB"}},{"@type":"WebSite","@id":"https://aventus.io/#website","url":"https://aventus.io","name":"Aventus","publisher":{"@id":"https://aventus.io/#person"},"inLanguage":"en-GB","potentialAction":{"@type":"SearchAction","target":"https://aventus.io/?s={search_term_string}","query-input":"required name=search_term_string"}},{"@type":"ImageObject","@id":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png","url":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png","width":"1920","height":"892","inLanguage":"en-GB"},{"@type":"WebPage","@id":"https://aventus.io/#webpage","url":"https://aventus.io/","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","datePublished":"2024-10-30T12:34:27+00:00","dateModified":"2025-01-28T13:28:27+00:00","about":{"@id":"https://aventus.io/#person"},"isPartOf":{"@id":"https://aventus.io/#website"},"primaryImageOfPage":{"@id":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png"},"inLanguage":"en-GB"},{"@type":"Person","@id":"https://aventus.io/author/396521c72263a9ca/","name":"500 Designs","url":"https://aventus.io/author/396521c72263a9ca/","image":{"@type":"ImageObject","@id":"https://secure.gravatar.com/avatar/7187f9812073ad5e55f95237c80ba5b5?s=96&amp;d=mm&amp;r=g","url":"https://secure.gravatar.com/avatar/7187f9812073ad5e55f95237c80ba5b5?s=96&amp;d=mm&amp;r=g","caption":"500 Designs","inLanguage":"en-GB"},"sameAs":["https://www.500designs.com"]},{"headline":"Aventus | Enterprise Blockchain-as-a-Service Solutions","description":"Blockchain, Digital Assets, and Tokenisation: Effortlessly Powerful. Enterprise-Grade.","datePublished":"2024-10-30T12:34:27+00:00","dateModified":"2025-01-28T13:28:27+00:00","keywords":"blockchain as a service,Enterprise blockchain solutions","image":{"@id":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png"},"author":{"@id":"https://aventus.io/author/396521c72263a9ca/","name":"500 Designs"},"@type":"Article","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","@id":"https://aventus.io/#schema-15419","isPartOf":{"@id":"https://aventus.io/#webpage"},"publisher":{"@id":"https://aventus.io/#person"},"inLanguage":"en-GB","mainEntityOfPage":{"@id":"https://aventus.io/#webpage"}},{"@type":"VideoObject","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","description":"Blockchain, Digital Assets, and Tokenisation: Effortlessly Powerful. Enterprise-Grade.","uploadDate":"2024-10-30T12:34:27+00:00","thumbnailUrl":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png","contentUrl":"https://aventuscdn.b-cdn.net/web/2024/animations/Hero%20Animation-High%20Quality.mp4","isFamilyFriendly":"True","@id":"https://aventus.io/#schema-15420","isPartOf":{"@id":"https://aventus.io/#webpage"},"publisher":{"@id":"https://aventus.io/#person"},"inLanguage":"en-GB","mainEntityOfPage":{"@id":"https://aventus.io/#webpage"}},{"@type":"WebSite","url":"https://aventus.io/","potentialAction":{"@type":"SearchAction","target":"https://aventus.io/?q={search_term_string}","query-input":"required name=search_term_string"},"video":{"@type":"VideoObject","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","description":"Blockchain, Digital Assets, and Tokenisation: Effortlessly Powerful. Enterprise-Grade.","thumbnailUrl":"https://aventus.io/thumbnail.jpg","uploadDate":"2024-10-30T12:34:27+00:00","duration":"PT2M30S","contentUrl":"https://aventuscdn.b-cdn.net/web/2024/animations/Hero%20Animation-High%20Quality.mp4"}}]}
+
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    
+    
+    const jsonLD = {"@context":"https://schema.org","@graph":[{"@type":["Person","Organization"],"@id":"https://aventus.io/#person","name":"Aventus","logo":{"@type":"ImageObject","@id":"https://aventus.io/#logo","url":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","contentUrl":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","caption":"Aventus","inLanguage":"en-GB"},"image":{"@type":"ImageObject","@id":"https://aventus.io/#logo","url":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","contentUrl":"https://aventus.io/wp-content/uploads/2024/10/Aventus-Logo.svg","caption":"Aventus","inLanguage":"en-GB"}},{"@type":"WebSite","@id":"https://aventus.io/#website","url":"https://aventus.io","name":"Aventus","publisher":{"@id":"https://aventus.io/#person"},"inLanguage":"en-GB","potentialAction":{"@type":"SearchAction","target":"https://aventus.io/?s={search_term_string}","query-input":"required name=search_term_string"}},{"@type":"ImageObject","@id":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png","url":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png","width":"1920","height":"892","inLanguage":"en-GB"},{"@type":"WebPage","@id":"https://aventus.io/#webpage","url":"https://aventus.io/","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","datePublished":"2024-10-30T12:34:27+00:00","dateModified":"2025-01-28T13:28:27+00:00","about":{"@id":"https://aventus.io/#person"},"isPartOf":{"@id":"https://aventus.io/#website"},"primaryImageOfPage":{"@id":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png"},"inLanguage":"en-GB"},{"@type":"Person","@id":"https://aventus.io/author/396521c72263a9ca/","name":"500 Designs","url":"https://aventus.io/author/396521c72263a9ca/","image":{"@type":"ImageObject","@id":"https://secure.gravatar.com/avatar/7187f9812073ad5e55f95237c80ba5b5?s=96&amp;d=mm&amp;r=g","url":"https://secure.gravatar.com/avatar/7187f9812073ad5e55f95237c80ba5b5?s=96&amp;d=mm&amp;r=g","caption":"500 Designs","inLanguage":"en-GB"},"sameAs":["https://www.500designs.com"]},{"headline":"Aventus | Enterprise Blockchain-as-a-Service Solutions","description":"Blockchain, Digital Assets, and Tokenisation: Effortlessly Powerful. Enterprise-Grade.","datePublished":"2024-10-30T12:34:27+00:00","dateModified":"2025-01-28T13:28:27+00:00","keywords":"blockchain as a service,Enterprise blockchain solutions","image":{"@id":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png"},"author":{"@id":"https://aventus.io/author/396521c72263a9ca/","name":"500 Designs"},"@type":"Article","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","@id":"https://aventus.io/#schema-15419","isPartOf":{"@id":"https://aventus.io/#webpage"},"publisher":{"@id":"https://aventus.io/#person"},"inLanguage":"en-GB","mainEntityOfPage":{"@id":"https://aventus.io/#webpage"}},{"@type":"VideoObject","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","description":"Blockchain, Digital Assets, and Tokenisation: Effortlessly Powerful. Enterprise-Grade.","uploadDate":"2024-10-30T12:34:27+00:00","thumbnailUrl":"https://aventus.io/wp-content/uploads/2025/01/1-updated-with-white-bg.png","contentUrl":"https://aventuscdn.b-cdn.net/web/2024/animations/Hero%20Animation-High%20Quality.mp4","isFamilyFriendly":"True","@id":"https://aventus.io/#schema-15420","isPartOf":{"@id":"https://aventus.io/#webpage"},"publisher":{"@id":"https://aventus.io/#person"},"inLanguage":"en-GB","mainEntityOfPage":{"@id":"https://aventus.io/#webpage"}},{"@type":"WebSite","url":"https://aventus.io/","potentialAction":{"@type":"SearchAction","target":"https://aventus.io/?q={search_term_string}","query-input":"required name=search_term_string"},"video":{"@type":"VideoObject","name":"Aventus | Enterprise Blockchain-as-a-Service Solutions","description":"Blockchain, Digital Assets, and Tokenisation: Effortlessly Powerful. Enterprise-Grade.","thumbnailUrl":"https://aventus.io/thumbnail.jpg","uploadDate":"2024-10-30T12:34:27+00:00","duration":"PT2M30S","contentUrl":"https://aventuscdn.b-cdn.net/web/2024/animations/Hero%20Animation-High%20Quality.mp4"}}]}
+    
     return (
+        
+        
         <>
             {/*<meta name="twitter:label1" content="Written by"/>*/}
             {/*<meta name="twitter:data1" content="twitterdata1"/>*/}
@@ -129,7 +136,7 @@ export default function RootLayout({
             {/*<meta name="article:author" content="article Author"/>*/}
             {/*<meta property="og:updated_time" content="2025-01-28T13:28:27+00:00"/>*/}
             
-            {/*<head title="Site Title">*/}
+            <head>
             {/*    <meta property="og:title" content="Site Title"/>*/}
             {/*    <meta property="og:type" content="website"/>*/}
             {/*    <meta property="og:url" content="http://my.site.com"/>*/}
@@ -140,12 +147,12 @@ export default function RootLayout({
             {/*    */}
             {/*    <meta name="twitter:card" content="summary_large_image"/>*/}
             {/*    */}
-            {/*    <script type="application/ld+json"*/}
-            {/*            className="rank-math-schema-pro"*/}
-            {/*            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLD) }}*/}
-            {/*    />*/}
+                <script type="application/ld+json"
+                        className="rank-math-schema-pro"
+                        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLD) }}
+                />
             {/*    <title>TItle</title>*/}
-            {/*</head>*/}
+            </head>
             <html lang="en">
                 
                 <body className={`${inter.className} antialiased h-dvh`}>
