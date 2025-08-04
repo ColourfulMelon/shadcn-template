28ffbf78e03a9b0448bef50647c7e35aa9429d71
ColourfulMelon <jordynnitch@gmail.com>
2025-08-04 12:27:36 +0200

    update next and zod

--- lib/client-env.ts
+++ lib/client-env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const clientEnvSchema = z.object({
-    NEXT_PUBLIC_SITE_URL: z.string().url(),
+    NEXT_PUBLIC_SITE_URL: z.url(),
     // todo populate with public environment variables
     
 });
