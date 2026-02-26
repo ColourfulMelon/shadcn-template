416b51ad22edfd0703118001002b12f4872395f8
ColourfulMelon <jordynnitch@gmail.com>
2026-02-26 21:48:06 +0200

    update packages and env vars for url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.NEXTAUTH_URL,
+	siteUrl: process.env.VERCEL_PROJECT_PRODUCTION_URL,
 	generateRobotsTxt: true,
 }
